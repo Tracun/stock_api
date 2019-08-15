b860bad754ff2711e8d656412e6da5b7668badde
tracun <lucas060994@gmail.com>
2019-08-15 08:10:17 -0300

    new function to register new tickers

--- responseConverter.py
+++ responseConverter.py
@@ -1,21 +1,29 @@
-import json
-
 class ResponseConverter:
 
-    linkGoogleFinance = 'http://www.google.com/googlefinance/disclaimer/';
+    linkGoogleFinance = 'http://www.google.com/googlefinance/disclaimer/'
+    URI = 'http://tracun.com/finance/api/v1/stocks/'
 
     def __init__(self):
         pass
 
     def tickers(self, tickerList):
-        return {'status':'200','assets':tickerList, 'about_stock_market_data':self.linkGoogleFinance}
+        return {'status':200,'assets':tickerList, 'about_stock_market_data':self.linkGoogleFinance}
 
     def ticker(self, ticker):
-        return {'status':'200','ticker':ticker, 'about_stock_market_data':self.linkGoogleFinance}
+        return {'status':200,'ticker':ticker, 'about_stock_market_data':self.linkGoogleFinance}
 
     def jsonInternalError(self, internalMessage):
-        return {'errors':[{'code':500,'message':'Internal Server Error', 'internalMessage':internalMessage}]}
+        return {'errors':[{'code':500,'message':'Internal Server Error', 'internalMessage':'{}'.format(internalMessage)}]}
 
     def tickerNotFound(self, ticker):
-        return {'errors':[{'code':404,'message':'Not Found', 'message':'Ticker not found', 'ticker':ticker}]}
+        return {'errors':[{'code':404,'message':'Not Found', 'message error':'Ticker not found', 'ticker':ticker}]}
+
+    def tickerNotExists(self, ticker):
+        return {'errors':[{'code':400,'message':'Not exists', 'message error':'Ticker not exists', 'ticker':ticker}]}
+
+    def tickerAlreadyExists(self, ticker):
+        return {'errors':[{'code':200,'message':'Ticker already exists','uri':'{}{}'.format(self.URI, ticker), 'ticker':ticker}]}
+
+    def tickerCreated(self, ticker):
+        return {'status':201,'uri':'{}{}'.format(self.URI, ticker), 'message':'Created', 'about_stock_market_data':self.linkGoogleFinance}
 
--- tickerRequest.py
+++ tickerRequest.py
@@ -2,10 +2,10 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from tickerObj import TickerObj
 from responseConverter import ResponseConverter
-import json
 
 class TickerRequest:
 
+    SHEET_NAME = 'TestePython'
     scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
     creds = ServiceAccountCredentials.from_json_keyfile_name('./data/client_secret.json', scope)
     client = gspread.authorize(creds)
@@ -17,10 +17,11 @@ class TickerRequest:
 
         global client
         global responseConverter
-        responseConverter = ResponseConverter()
+        global SHEET_NAME
 
+        responseConverter = ResponseConverter()
         try:
-            sheet = self.client.open('TestePython').sheet1
+            sheet = self.client.open(self.SHEET_NAME).sheet1
         except Exception as e:
             return responseConverter.jsonInternalError(e)
 
@@ -28,33 +29,38 @@ class TickerRequest:
         cell_list = sheet.range('d2:k992')
         tickerList = []
 
-        for x in range(0, len(cell_list), 8):
+        try:
+            for x in range(0, len(cell_list), 8):
 
-            if cell_list[x].value == '':
-                break
-            else:
-                tickerObj = TickerObj(cell_list[x].value, cell_list[x+1].value, cell_list[x+2].value, cell_list[x+3].value,
-                        cell_list[x+4].value, cell_list[x+5].value, cell_list[x+6].value, cell_list[x+7].value)
+                if cell_list[x].value == '':
+                    break
+                else:
+                    tickerObj = TickerObj(cell_list[x].value, cell_list[x+1].value, cell_list[x+2].value, cell_list[x+3].value,
+                            cell_list[x+4].value, cell_list[x+5].value, cell_list[x+6].value, cell_list[x+7].value)
 
-                #add obj to list
-                tickerList.append(tickerObj)
+                    #add obj to list
+                    tickerList.append(tickerObj)
 
-        #Return the obj to dict
-        results = [obj.to_dict() for obj in tickerList]
+            #Return the obj to dict
+            results = [obj.to_dict() for obj in tickerList]
 
-        #sort it based on ticker
-        results.sort(key=lambda obj: obj["ticker"])
+            #sort it based on ticker
+            results.sort(key=lambda obj: obj["ticker"])
 
-        return responseConverter.tickers(results)
+            return responseConverter.tickers(results)
+        except Exception as e:
+            print('Erro: ', e)
 
     def getTicker(self, ticker):
         global client
         global responseConverter
+        global SHEET_NAME
+
         responseConverter = ResponseConverter()
         ticker.upper()
 
         try:
-            sheet = self.client.open('TestePython').sheet1
+            sheet = self.client.open(self.SHEET_NAME).sheet1
         except Exception as e:
             return responseConverter.jsonInternalError(e)
 
@@ -77,4 +83,37 @@ class TickerRequest:
         except Exception as notFound:
             return responseConverter.tickerNotFound(ticker)
 
+    def newTicker(self, ticker):
+        global client
+        global responseConverter
+        global SHEET_NAME
 
+        responseConverter = ResponseConverter()
+        ticker.upper()
+
+        try:
+            sheet = self.client.open(self.SHEET_NAME).sheet1
+        except Exception as e:
+            return responseConverter.jsonInternalError(e)
+
+        try:
+
+            cell = sheet.find(ticker.upper())
+            return responseConverter.tickerAlreadyExists(ticker)
+
+        except Exception as notFound:
+            #TERMINAR, SE DER ERRO, SIGNIFICA QUE NAO EXISTE, ENTAO VAMOS CRIAR
+            line = int(sheet.acell('B12').value)
+            cellToInsert = 'D{}'.format(line)
+
+            sheet.update_acell(cellToInsert, ticker)
+
+            #check if the ticket is valid
+            if(str(sheet.acell('H{}'.format(line)).value) == 'Not found'):
+                sheet.update_acell(cellToInsert, '')
+                return responseConverter.tickerNotExists(ticker)
+
+            #update the number of tickers registered
+            line += 1
+            sheet.update_acell('B12', str(line))
+            return responseConverter.tickerCreated(ticker)
