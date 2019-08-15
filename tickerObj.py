0aae12b621482cc7816f7c7822b83bb1c794bb7c
BASTOLU <lucas060994@gmi>
2019-08-15 08:12:18 -0300

    add uri attribute

--- tickerObj.py
+++ tickerObj.py
@@ -1,9 +1,11 @@
 class TickerObj():
 
+    URI = 'http://tracun.com/finance/api/v1/stocks/'
+
     def __init__(self):
         pass
 
-    def __init__(self, ticker, price, percentChange, volume, currency, high, low, dateDelay):
+    def __init__(self, ticker, price, percentChange, volume, currency, high, low, dateDelay, uri=URI):
         self.ticker = ticker
         self.price = price
         self.percentChange = percentChange
@@ -11,9 +13,10 @@ class TickerObj():
         self.currency = currency
         self.high = high
         self.low = low
+        self.uri = uri + ticker
         self.dateDelay = dateDelay
 
     def to_dict(self):
         return {"ticker": self.ticker, "price": self.price, "percentChange": self.percentChange, "volume": self.volume, "currency": self.currency,
-                    "high": self.high, "low": self.low, "dateDelay": self.dateDelay}
+                    "high": self.high, "low": self.low, "uri": self.uri, "dateDelay": self.dateDelay}
 
