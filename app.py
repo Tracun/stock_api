e473bcc22fe01a7777a335d65ce2dbc8bebafb64
BASTOLU <lucas060994@gmi>
2019-08-15 08:25:28 -0300

    new function to register new tickers

--- app.py
+++ app.py
@@ -8,15 +8,14 @@ api = Api(app)  # api is a collection of objects, where each object contains a s
 def start():
     app.run(host='0.0.0.0', port=5000)
 
-@app.route('/finance/api/v1/stocks')
+@app.route('/finance/api/stocks')
 def AllStockss():
     tickerRequest = TickerRequest()
 
     response = tickerRequest.getTickers()
     return jsonify(response)
 
-
-@app.route('/finance/api/v1/stocks/<string:ticker>')
+@app.route('/finance/api/stocks/<string:ticker>')
 def Ticker(ticker):
     tickerRequest = TickerRequest()
     response = tickerRequest.getTicker(ticker)
@@ -24,14 +23,15 @@ def Ticker(ticker):
 
 class NewTicker(Resource):
     def post(self, ticker):  # param is pulled from url string
+        tickerRequest = TickerRequest()
 
-	    return None
+        response = tickerRequest.newTicker(ticker)
+        return jsonify(response)
 
 @app.route('/')
 @app.route('/home')
 @app.route('/finance')
 @app.route('/finance/api')
-@app.route('/finance/api/v1')
 def index():
     return render_template(
         'index.html',
@@ -39,7 +39,7 @@ def index():
     )
 
 # once we've defined our api functionalities, add them to the master API object
-api.add_resource(NewTicker, '/finance/api/v1/newTicker/<string:ticker>')
+api.add_resource(NewTicker, '/finance/api/stocks/<string:ticker>')
 
 if __name__ == '__main__':
     #app.run(debug=True)
